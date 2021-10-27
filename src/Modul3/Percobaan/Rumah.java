387e52a0f671f6017a85e4101eeecdc123290279
Daffarr <77198942+xxBumblebee@users.noreply.github.com>
2021-10-27 23:14:46 +0700

    add latihan

---
+++ src/Modul3/Percobaan/Rumah.java
@@ -0,0 +1,9 @@
+package Modul3.Percobaan;
+
+public class Rumah {
+    String alamat_rumah;
+
+    public void setAlamat(String x) {
+        alamat_rumah = x;
+    }
+}
---
+++ src/Modul3/Percobaan/Test.java
@@ -0,0 +1,9 @@
+package Modul3.Percobaan;
+
+public class Test {
+    public static void main(String[] args) {
+        Rumah r = new Rumah();
+        r.setAlamat("Alamat saya di jalan Payanibung");
+        System.out.println(r.alamat_rumah);
+    }
+}
