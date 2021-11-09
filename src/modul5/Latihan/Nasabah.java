286fed17cd9c0c77eb0a290ac91b438b54b67769
Daffarr <77198942+xxBumblebee@users.noreply.github.com>
2021-11-09 19:09:46 +0700

    update modul 5 latihan

---
+++ src/modul5/Latihan/Nasabah.java
@@ -0,0 +1,16 @@
+package com.daffa.pbo.modul5.latihan;
+
+public class Nasabah {
+    String namaAwal;
+    String namaAkhir;
+    Tabungan tabungan;
+
+    Nasabah(String namaAwal, String namaAkhir) {
+        this.namaAwal = namaAwal;
+        this.namaAkhir = namaAkhir;
+    }
+
+    public void setTabungan(Tabungan tabungan) {
+
+    }
+}
