de3be656d19b6974f40bcb804a80c4b9fe014066
Daffarr <77198942+xxBumblebee@users.noreply.github.com>
2021-11-18 17:41:23 +0700

    Revert "delete modul 2"
    This reverts commit 63b3acf4e971fac31596d1b9b4a82b58f4039f9a.

---
+++ src/Modul2/Percobaan/Aritmatika.java
@@ -0,0 +1,29 @@
+package Modul2.Percobaan;
+
+public class Aritmatika {
+    public static void main(String[] args) {
+        System.out.println("Operasi Aritmatika "+"Pada tipe Integer");
+        int a = 2+1;
+        int b = a-1;
+        int c = a*b;
+        int d = c/3;
+        int e = -a;
+        System.out.println("a : " +a);
+        System.out.println("b : " +b);
+        System.out.println("c : " +c);
+        System.out.println("d : " +d);
+        System.out.println("e : " +e);
+
+        System.out.println("Operasi Aritmatika "+"Pada tipe floating-point");
+        double fa = 2+1;
+        double fb = fa-1;
+        double fc = fa*fb;
+        double fd = fc/3;
+        double fe = -fa;
+        System.out.println("fa : " +fa);
+        System.out.println("fb : " +fb);
+        System.out.println("fc : " +fc);
+        System.out.println("fd : " +fd);
+        System.out.println("fe : " +fe);
+    }
+}
